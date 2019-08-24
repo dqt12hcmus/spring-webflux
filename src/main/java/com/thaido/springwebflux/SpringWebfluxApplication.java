101b94a83202209d692881b4a2d9141f80e96f18
thai.do <thai.do@codelink.io>
2019-08-24 17:30:29 +0700

    create new client

--- src/main/java/com/thaido/springwebflux/SpringWebfluxApplication.java
+++ src/main/java/com/thaido/springwebflux/SpringWebfluxApplication.java
@@ -1,5 +1,6 @@
 package com.thaido.springwebflux;
 
+import com.thaido.springwebflux.client.GreetingWebClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class SpringWebfluxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebfluxApplication.class, args);
+
+		GreetingWebClient client = new GreetingWebClient();
+    System.out.println(client.getResult());
 	}
 
 }
