bb6574ca21c4378c8168df4c247a960ff02fb33d
thai.do <thai.do@codelink.io>
2019-08-24 17:29:49 +0700

    add client to consume

--- src/main/java/com/thaido/springwebflux/client/GreetingWebClient.java
+++ src/main/java/com/thaido/springwebflux/client/GreetingWebClient.java
@@ -1,5 +1,22 @@
 package com.thaido.springwebflux.client;
 
-public class GreetingWebClient {
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+public class GreetingWebClient {
+	private WebClient client = WebClient.create("http://localhost:8080");
+
+	private Mono<ClientResponse> result =
+		client.get()
+		.uri("/hello")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange();
+
+	public String getResult() {
+		return ">> result: " + result.flatMap(
+			result -> result.bodyToMono(String.class)
+		).block();
+	}
 }
