f263263cc72b41b1d5569809612773e4857e93b2
thai.do <thai.do@codelink.io>
2019-08-24 17:29:22 +0700

    add router

--- src/main/java/com/thaido/springwebflux/router/GreetingRouter.java
+++ src/main/java/com/thaido/springwebflux/router/GreetingRouter.java
@@ -1,5 +1,23 @@
 package com.thaido.springwebflux.router;
 
-public class GreetingRouter {
+import com.thaido.springwebflux.handler.GreetingHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
+@Configuration
+public class GreetingRouter {
+	@Bean
+	public RouterFunction<ServerResponse> route(GreetingHandler handler) {
+
+		return RouterFunctions.route(
+			RequestPredicates.GET("/hello")
+			.and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+			handler::hello
+		);
+	}
 }
